31e9c7563b807c6f2ff98276c51e9197b36a9c9a
Sergey Travin <sergey.a.travin@gmail.com>
2014-04-13 21:07:37 +0600

    Unit-10: [sockets] Add code comments

--- Unit-10/src/com/practicaljava/unit_10/sockets/SimpleSocketClient.java
+++ Unit-10/src/com/practicaljava/unit_10/sockets/SimpleSocketClient.java
@@ -6,22 +6,31 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * The simple socket client connects to the server, gets user input
+ * and sends it to the server.
+ */
 public class SimpleSocketClient {
 
 	public static void main(String[] args) throws IOException {
 		String hostName = "localhost";
 		int portNumber = 4000;
 
+		// 1. Creates a Socket instance
+		// 2. Gets input and output streams from this socket
 		try (Socket clientSocket = new Socket(hostName, portNumber);
 				BufferedReader in = new BufferedReader(new InputStreamReader(
 						clientSocket.getInputStream()));
 				PrintWriter out = new PrintWriter(
 						clientSocket.getOutputStream(), true);) {
 
-			String inputLine, outputLine;
-
+			// Gets the standard input stream
 			BufferedReader stdIn = new BufferedReader(new InputStreamReader(
 					System.in));
+
+			// Gets user input and writes it to the output
+			// stream (sends to the server).
+			String inputLine;
 			while ((inputLine = stdIn.readLine()) != null) {
 				out.println(inputLine);
 			}
--- Unit-10/src/com/practicaljava/unit_10/sockets/SimpleSocketServer.java
+++ Unit-10/src/com/practicaljava/unit_10/sockets/SimpleSocketServer.java
@@ -7,11 +7,18 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * The simple socket server listens and accepts a single
+ * client connection, and prints out messages from it.
+ */
 public class SimpleSocketServer {
 
 	public static void main(String[] args) throws IOException {
 		int portNumber = 4000;
 
+		// 1. Creates a ServerSocket instance
+		// 2. Waits for a client connection, and then gets a Socket
+		// 3. Gets input and output streams from this socket
 		try (ServerSocket serverSocket = new ServerSocket(portNumber);
 				Socket clientSocket = serverSocket.accept();
 				BufferedReader in = new BufferedReader(new InputStreamReader(
@@ -19,8 +26,8 @@ public class SimpleSocketServer {
 				PrintWriter out = new PrintWriter(
 						clientSocket.getOutputStream(), true);) {
 
-			String inputLine, outputLine;
-
+			// Prints out receiving lines
+			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
 				System.out.println(inputLine);
 			}
