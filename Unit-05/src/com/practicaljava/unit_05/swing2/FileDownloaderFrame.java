d9863dc1aeddfa204da15fb21b4b14b4e5102ca9
Sergey Travin <sergey.a.travin@gmail.com>
2014-04-26 18:00:15 +0600

    Unit-05: Add SwingWorker in FileDownloader

--- Unit-05/src/com/practicaljava/unit_05/swing2/FileDownloaderFrame.java
+++ Unit-05/src/com/practicaljava/unit_05/swing2/FileDownloaderFrame.java
@@ -3,12 +3,16 @@ package com.practicaljava.unit_05.swing2;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.BufferedInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,12 +20,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
 import javax.swing.border.EmptyBorder;
 
 /**
  * A file downloader frame.
  */
 public class FileDownloaderFrame extends JFrame {
+	private final JProgressBar progressBar;
+	private final JLabel labelStatus;
 
 	public FileDownloaderFrame(String title) {
 		super(title);
@@ -50,41 +57,59 @@ public class FileDownloaderFrame extends JFrame {
 
 		// Progress bar
 
-		JProgressBar progressBar = new JProgressBar(0, 100);
+		progressBar = new JProgressBar(0, 100);
 
 		mainPanel.add(progressBar, BorderLayout.CENTER);
 
 		// Status label
 
-		final JLabel labelStatus = new JLabel("No actions");
+		labelStatus = new JLabel("No actions");
 		mainPanel.add(labelStatus, BorderLayout.SOUTH);
-		
+
 		// Sets actions
-		
+
 		buttonGet.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String url = textURL.getText();
-				labelStatus.setText("Downloading...");
-				download(url);
-				labelStatus.setText("Done");
+
+				setStatus("Downloading...");
+
+				DownloadTask task = new DownloadTask(url);
+				task.addPropertyChangeListener(new PropertyChangeListener() {
+
+					@Override
+					public void propertyChange(PropertyChangeEvent evt) {
+						if ("progress".equals(evt.getPropertyName())) {
+							progressBar.setValue((Integer) evt.getNewValue());
+						}
+					}
+				});
+				task.execute();
 			}
 		});
-		
+
 		// Sizes the frame
 		pack();
 	}
 
-	private void download(String fromURL) {
+	private void setStatus(String status) {
+		labelStatus.setText(status);
+	}
+
+	private void download(String fromURL) throws IOException {
 		String toFile = "downloaded_file";
 
 		System.out.println("Downloading. Please wait...");
 
 		try {
 			URL url = new URL(fromURL);
+			URLConnection conn = url.openConnection();
+			System.out
+					.println("Content-Length: " + conn.getContentLengthLong());
 
 			try (BufferedInputStream in = new BufferedInputStream(
-					url.openStream());
+					conn.getInputStream());
 					FileOutputStream fout = new FileOutputStream(toFile)) {
 				int data;
 				while ((data = in.read()) != -1) {
@@ -104,5 +129,28 @@ public class FileDownloaderFrame extends JFrame {
 		}
 
 		System.out.println(fromURL + " -> " + toFile + " OK");
+	}
+
+	private class DownloadTask extends SwingWorker<Void, String> {
+		private final String fromUrl;
+
+		public DownloadTask(String fromUrl) {
+			this.fromUrl = fromUrl;
+		}
+
+		@Override
+		protected Void doInBackground() throws Exception {
+			download(fromUrl);
+			return null;
+		}
+
+		@Override
+		protected void process(List<String> chunks) {
+		}
+
+		@Override
+		protected void done() {
+			setStatus("Done.");
+		}
 	}
 }
