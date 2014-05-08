555db84bed10721ef86996d0caa1d1530eb25f43
Sergey Travin <sergey.a.travin@gmail.com>
2014-05-09 00:31:16 +0600

    Unit-13: [jtable] Implement AbstractTableModel

--- Unit-13/src/com/practicaljava/unit_13/jtable/FrameWithTable.java
+++ Unit-13/src/com/practicaljava/unit_13/jtable/FrameWithTable.java
@@ -1,5 +1,7 @@
 package com.practicaljava.unit_13.jtable;
 
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -21,25 +23,42 @@ public class FrameWithTable extends JFrame {
 		add(new JScrollPane(myTable));
 	}
 
-	public class MyTableModel extends AbstractTableModel {
+	private class MyTableModel extends AbstractTableModel {
 		private static final long serialVersionUID = 1L;
+
+		private ArrayList<Order> myData = new ArrayList<>();
+
+		MyTableModel() {
+			myData.add(new Order(1, "IBM", 100, 135.5));
+			myData.add(new Order(2, "AAPL", 300, 290.12));
+			myData.add(new Order(3, "MOT", 2000, 8.32));
+			myData.add(new Order(4, "ORCL", 500, 27.8));
+		}
 
 		@Override
 		public int getColumnCount() {
-			// TODO Auto-generated method stub
-			return 0;
+			return 4;
 		}
 
 		@Override
 		public int getRowCount() {
-			// TODO Auto-generated method stub
-			return 0;
+			return myData.size();
 		}
 
 		@Override
-		public Object getValueAt(int arg0, int arg1) {
-			// TODO Auto-generated method stub
-			return null;
+		public Object getValueAt(int row, int col) {
+			switch (col) {
+			case 1:
+				return myData.get(row).getOrderID();
+			case 2:
+				return myData.get(row).getStockSymbol();
+			case 3:
+				return myData.get(row).getQuantity();
+			case 4:
+				return myData.get(row).getPrice();
+			default:
+				return "";
+			}
 		}
 	}
 
