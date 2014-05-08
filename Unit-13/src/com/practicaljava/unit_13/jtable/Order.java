aa740558004a9d8ebe24b39cbeb9c1aee1a1b880
Sergey Travin <sergey.a.travin@gmail.com>
2014-05-09 00:11:41 +0600

    Unit-13: [jtable] Add Order.java

---
+++ Unit-13/src/com/practicaljava/unit_13/jtable/Order.java
@@ -0,0 +1,15 @@
+package com.practicaljava.unit_13.jtable;
+
+public class Order {
+	private int orderID;
+	private String stockSymbol;
+	private int quantity;
+	private double price;
+
+	public Order(int id, String stockSymbol, int quantity, double price) {
+		this.orderID = id;
+		this.stockSymbol = stockSymbol;
+		this.quantity = quantity;
+		this.price = price;
+	}
+}
